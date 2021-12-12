cff49f572605356f5b2011ac8470748e356b5cd5
Koushik Sadhukhan <kousadhu@WKMZT4EDA301.global.publicisgroupe.net>
2021-12-12 17:30:51 +0530

    adding comments

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -2,6 +2,7 @@ import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
 
 export const MarketingApp = () => {
+  // mounting the micro app
   const marketingRef = useRef(null);
 
   useEffect(() => {
